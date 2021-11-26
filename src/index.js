74b4361ac7c4800a229ded3340819a5385efd4ca
sabarishs19 <84761863+sabarishs19@users.noreply.github.com>
2021-11-26 11:30:48 +0530

    init

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Canvas } from "p5-react-renderer";
+import ParticleSystem from "./ParticleSystem";
+
+function App() {
+  return (
+    <Canvas size={[750, 750]} background="#231f20" noStroke noClear>
+      <colorMode args={(p5) => [p5.RGB]}>
+        <ParticleSystem num={300} />
+      </colorMode>
+    </Canvas>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
